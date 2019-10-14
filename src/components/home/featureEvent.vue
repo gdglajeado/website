b93a5eb4351622175bb32d74d4488c80600b740e
Rafael Dutra <raffaeldutra@gmail.com>
2019-10-14 20:29:16 -0300

    Envio de projeto

--- src/components/home/featureEvent.vue
+++ src/components/home/featureEvent.vue
@@ -33,7 +33,7 @@
                    {{eventDetails.EventDescription}}
                </p>
                 
-                <v-btn color="#1a73e8" v-if="eventDetails.RegistrationLink.length>0" :href="eventDetails.RegistrationLink" target="_blank" class="ma-0 elevation-0 my-2" dark style="text-transform: capitalize;border-radius:5px;"> 
+                <v-btn color="#1a73e8" v-if="eventDetails.RegistrationLink.length>0" :href="eventDetails.RegistrationLink" target="_blank" class="ma-0 elevation-0 my-2" dark style="text-transform: border-radius:5px;"> 
                     Registre-se
                 </v-btn>
                 &nbsp;
