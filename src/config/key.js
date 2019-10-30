e0d8e2344c181254cb1e8b1361f756d900cb4966
Rafael Dutra <raffaeldutra@gmail.com>
2019-10-30 13:25:43 -0300

    Testing

--- src/config/key.js
+++
@@ -1,5 +0,0 @@
-export const MeetupAPI = {
-  urlname: "GDG Lajeado" // Example GDG-Jalandhar  CHAPTER_URL_NAME
-};
-
-// For urlname: extract the urlname from meetup chapter link-> https://meetup.com/GDG-Jalandhar
